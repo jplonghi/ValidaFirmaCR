462c0e94142fb57181965400c825ed4caccfb9ea
Jose Longhi <jose.longhi@crengland.com>
2019-05-27 12:49:31 -0600

    Mostar cedula, nombre y apellidos
    Uso de OIDs
    Archivo inicial de constantes OIDs

--- src/Program.cs
+++ src/Program.cs
@@ -7,23 +7,32 @@ namespace validador
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
             PdfReader reader = new PdfReader("../sample-docs/my_signed_doc.pdf");
-            foreach(string signatureName in reader.AcroFields.GetSignatureNames()){
-                
-                 var signatureDict = reader.AcroFields.GetSignatureDictionary(signatureName);
-                 PdfPkcs7 pk  =  reader.AcroFields.VerifySignature(signatureName);
-                 var certs = pk.Certificates;
+            foreach (string signatureName in reader.AcroFields.GetSignatureNames())
+            {
 
-                 Console.WriteLine("Firmado por: "+pk.SignName);
-                 Console.WriteLine("Firma Verificada: "+pk.Verify());
-                
+                var signatureDict = reader.AcroFields.GetSignatureDictionary(signatureName);
+                PdfPkcs7 pk = reader.AcroFields.VerifySignature(signatureName);
+                var certs = pk.Certificates;
 
-                 
+                string nombreCertificado = pk.Certificates[0].SubjectDN.GetValueList(new Org.BouncyCastle.Asn1.DerObjectIdentifier(Constantes.OID_COMMON_NAME))[0].ToString();
+                string cedula = pk.Certificates[0].SubjectDN.GetValueList(new Org.BouncyCastle.Asn1.DerObjectIdentifier(Constantes.OID_SERIAL_NUMBER))[0].ToString();
+                string nombre = pk.Certificates[0].SubjectDN.GetValueList(new Org.BouncyCastle.Asn1.DerObjectIdentifier(Constantes.OID_GIVEN_NAME))[0].ToString();
+                string apellidos = pk.Certificates[0].SubjectDN.GetValueList(new Org.BouncyCastle.Asn1.DerObjectIdentifier(Constantes.OID_SURNAME))[0].ToString();
+
+                Console.WriteLine("Firma Verificada: " + pk.Verify());
+                Console.WriteLine($"Nombre del certificado: {nombreCertificado}");
+                Console.WriteLine($"Cedula: {cedula}");
+                Console.WriteLine($"Nombre: {nombre}");
+                Console.WriteLine($"Apellidos: {apellidos}");
             }
-            
-           
+
+
         }
 
 
---
+++ src/constantes.cs
@@ -0,0 +1,36 @@
+using System;
+
+
+public static class Constantes
+{
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.5
+    /// </summary>
+    public const String OID_SERIAL_NUMBER = "2.5.4.5";
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.3
+    /// </summary>
+    public const String OID_COMMON_NAME = "2.5.4.3";
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.10
+    /// </summary>
+    public const String OID_ORGANIZATION_NAME = "2.5.4.10";
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.11
+    /// </summary>
+    public const String OID_ORGANIZATION_UNIT_NAME = "2.5.4.11";
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.4
+    /// </summary>
+    public const String OID_SURNAME = "2.5.4.4";
+
+    /// <summary>
+    /// https://oidref.com/2.5.4.42
+    /// </summary>
+    public const String OID_GIVEN_NAME = "2.5.4.42";
+}
